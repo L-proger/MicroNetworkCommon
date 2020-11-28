229f7b364c03c17e79c81023eb1ce6edddc1c20c
l-proger <l-proger@yandex.ru>
2020-11-28 16:25:45 +0300

    Move files from LFramework

---
+++ Src/MicroNetwork/Common/Packet.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <cstdint>
+#include <LFramework/BitField.h>
+#include <array>
+
+namespace MicroNetwork::Common {
+
+union PacketDescription {
+    std::uint8_t data;
+    LFramework::BitFieldMember<std::uint8_t, 0, 1> isService;
+    LFramework::BitFieldMember<std::uint8_t, 1, 7> reserved;
+};
+
+static_assert(sizeof(PacketDescription) == 1, "Invalid PacketDescription size");
+
+struct PacketId {
+    static constexpr std::uint8_t Bind = 0xff;
+    static constexpr std::uint8_t Unbind = 0xfe;
+    static constexpr std::uint8_t TaskStart = 0xfd;
+    static constexpr std::uint8_t TaskStop = 0xfc;
+};
+
+struct PacketHeader {
+    std::uint8_t id;
+    std::uint8_t size;
+
+    std::size_t fullSize() const {
+    	return sizeof(PacketHeader) + size;
+    }
+};
+
+struct MaxPacket {
+       PacketHeader header;
+       std::array<std::uint8_t, 256> payload;
+   };
+
+}
